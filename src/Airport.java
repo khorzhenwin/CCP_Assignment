0815057230ecc0ca9b9e84dcc71c46394d97b0d9
Khor Zhen Win <khorzhenwin@gmail.com>
2022-05-26 14:51:41 +0800

    :cd: Mimic a somewhat concurrent loop

--- src/Airport.java
+++ src/Airport.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 
 public class Airport {
 
-   public Plane localPlane;
    int maxQueueSize = 6;
    List<Plane> planeQueue;
 
@@ -16,22 +15,23 @@ public class Airport {
    }
 
    void joinQueue(Plane plane) {
-      System.out.println("ATC: " + plane.getPlaneName() + " is requesting to enter the queue to land.");
+      System.out.println(plane.getPlaneName() + " is requesting to enter the queue to land.");
       synchronized (planeQueue) {
          if (planeQueue.size() == maxQueueSize) {
             System.out.println("ATC: " + "The queue is currently full." + plane.getPlaneName() + ", please refer to the next nearest airport to avoid running out of fuel.");
-            System.out.println("ATC: " + plane.getPlaneName() + " left the queue ...");
+            System.out.println(plane.getPlaneName() + " left the queue ...");
             return;
          }
          ((LinkedList<Plane>) planeQueue).offer(plane);
-         System.out.println("ATC: " + plane.getPlaneName() + " successfully joined the waiting queue to land.");
+         System.out.println(plane.getPlaneName() + " successfully joined the waiting queue to land.");
          if (planeQueue.size() == 1) {
             planeQueue.notify();
          }
       }
    }
 
-   void land(DockingGate gate, int gateNo) {
+   Plane land(DockingGate gate, int gateNo) {
+      Plane plane;
       synchronized (planeQueue) {
          while (planeQueue.size() == 0) {
             System.out.println("ATC: " + "Runway is empty. Waiting for planes.");
@@ -41,30 +41,31 @@ public class Airport {
                iex.printStackTrace();
             }
          }
-         System.out.println("ATC: " + ((Plane) ((LinkedList<?>) planeQueue).peekFirst()).getPlaneName() + " is requesting to use the runway");
-         localPlane = (Plane) ((LinkedList<?>) planeQueue).poll();
+         System.out.println(((Plane) ((LinkedList<?>) planeQueue).peekFirst()).getPlaneName() + " is requesting to use the runway");
+         plane = (Plane) ((LinkedList<?>) planeQueue).poll();
       }
 
       // if any gates are open, then allow to land
       long duration = 0;
       try {
-         System.out.println("ATC: " + localPlane.getPlaneName() + " is currently using the runway.");
+         System.out.println("ATC: " + plane.getPlaneName() + " is currently using the runway.");
          duration = (long) (Math.random() * 10);
          Thread.sleep(duration);
       } catch (InterruptedException iex) {
          iex.printStackTrace();
       }
-      System.out.println("ATC: " + localPlane.getPlaneName() + " has completed using the runway in " + duration + " seconds.");
+      System.out.println("ATC: " + plane.getPlaneName() + " has completed using the runway in " + duration + " seconds.");
+      System.out.println("ATC: " + plane.getPlaneName() + " please dock at gate " + gateNo);
       System.out.println("");
       System.out.println("ATC: " + "RUNWAY IS NOW AVAILABLE");
-      System.out.println("ATC: " + localPlane.getPlaneName() + " please dock at gate " + gateNo);
-      dock(localPlane, gate, gateNo);
+      dock(plane, gate, gateNo);
+      return plane;
    }
 
-   void depart(DockingGate gate, int gateNo) {
+   void depart(Plane localPlane, DockingGate gate, int gateNo) {
       long duration = 0;
       try {
-         System.out.println("ATC: " + localPlane.getPlaneName() + " is now departing from the airport.");
+         System.out.println(localPlane.getPlaneName() + " is now departing from the airport.");
          duration = (long) (Math.random() * 5);
          TimeUnit.SECONDS.sleep(duration);
       } catch (InterruptedException iex) {
@@ -84,6 +85,7 @@ public class Airport {
          System.out.println("ATC: " + plane.getPlaneName() + " is currently docking at gate " + gateNo);
          TimeUnit.SECONDS.sleep(duration);
          System.out.println("ATC: " + plane.getPlaneName() + " has successfully docked at gate " + gateNo + " ,and will now be unboarding passengers.");
+         // insert unboarding stuff here
       } catch (InterruptedException iex) {
          iex.printStackTrace();
       }
@@ -101,7 +103,7 @@ public class Airport {
          iex.printStackTrace();
       }
       System.out.println("ATC: " + plane.getPlaneName() + " is now using the runway to take off.");
-      System.out.println("ATC: " + plane.getPlaneName() + " has successfully departed.");
+      System.out.println(plane.getPlaneName() + " has successfully departed.");
       System.out.println("");
       gate.undock();
    }
--- src/Runway.java
+++ src/Runway.java
@@ -16,9 +16,13 @@ import java.util.logging.Logger;
  */
 public class Runway implements Runnable {
 
+   Plane planeAtGate1, planeAtGate2;
    Airport airport;
    DockingGate gate1, gate2;
    public volatile boolean takeOffPriority = false;
+   // 1 - plane left gate 1 recently
+   // 2 - plane left gate 2 recently
+   public volatile int lastToTakeOff = 0;
    boolean run = true;
 
    public Runway(Airport ATC, DockingGate gate1, DockingGate gate2) {
@@ -33,21 +37,21 @@ public class Runway implements Runnable {
       } catch (InterruptedException iex) {
          iex.printStackTrace();
       }
-      System.out.println("Runway is now open");
+      System.out.println("ATC: Runway is now open");
       while (run) {
          while (!takeOffPriority) {
             try {
                // if both gates are free, gate 1 will be used as 1st choice
                if ((!gate1.isOccupied() && gate2.isOccupied()) || (!gate1.isOccupied() && !gate2.isOccupied())) {
-                  System.out.println("Gate 1 is available");
-                  airport.land(gate1, 1);
+                  System.out.println("ATC: Gate 1 is available");
+                  planeAtGate1 = airport.land(gate1, 1);
                   notifyAll();
                } else if (gate1.isOccupied() && !gate2.isOccupied()) {
-                  System.out.println("Gate 2 is available");
-                  airport.land(gate2, 2);
+                  System.out.println("ATC: Gate 2 is available");
+                  planeAtGate2 = airport.land(gate2, 2);
                   notifyAll();
                } else {
-                  System.out.println("All docking gates are currently occupied. Please wait momentarily to land on the runway until a plane departs!");
+                  System.out.println("ATC: All docking gates are currently occupied. Please wait until a plane departs!");
                   setTakeOffPriority();
                   notifyAll();
                }
@@ -58,13 +62,15 @@ public class Runway implements Runnable {
          while (takeOffPriority) {
             try {
                Thread.sleep(5000);
-               if (gate1.isOccupied()) {
-                  airport.depart(gate1, 1);
+               if (gate1.isOccupied() && (lastToTakeOff == 2 || lastToTakeOff == 0)) {
+                  airport.depart(planeAtGate1, gate1, 1);
                   takeOffPriority = false;
+                  lastToTakeOff = 1;
                   notifyAll();
-               } else if (gate2.isOccupied()) {
-                  airport.depart(gate2, 2);
+               } else if (gate2.isOccupied() && lastToTakeOff == 1) {
+                  airport.depart(planeAtGate2, gate2, 2);
                   takeOffPriority = false;
+                  lastToTakeOff = 2;
                   notifyAll();
                }
             } catch (Exception e) {
